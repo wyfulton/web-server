ca3f88dd2e0438d8dcbffcff0e749773dcd3be0b
William Fulton <wyfulton@gmail.com>
2015-12-19 13:03:03 -0500

    updated language

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(middleware.logger);
 // app.use(middleware.requireAuthentication);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('this is the about page')
+	res.send('this is the about page!')
 });
 
 app.use(express.static(__dirname + "/public"));
