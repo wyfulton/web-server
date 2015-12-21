7b78947cae50551d2d6f471d4636eb1f16f9941f
William Fulton <wyfulton@gmail.com>
2015-12-21 15:20:21 -0500

    update port name

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.Port || 3000;
+var PORT = process.env.PORT || 3000;
 
 var middleware = require('./middleware.js');
 
@@ -13,6 +13,6 @@ app.get('/about', middleware.requireAuthentication, function (req, res) {
 app.use(express.static(__dirname + "/public"));
 
 
-app.listen(port, function () {
-	console.log('express server started on port ' + port);
+app.listen(PORT, function () {
+	console.log('express server started on PORT ' + PORT);
 });
