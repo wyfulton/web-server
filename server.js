951a59e6b0f4a4316499e5f2b3de24d94be2ef15
William Fulton <wyfulton@gmail.com>
2015-12-21 14:22:57 -0500

    refactoring

--- server.js
+++ server.js
@@ -2,16 +2,7 @@ var express = require('express');
 var app = express();
 var port = 3000
 
-var middleware = {
-	requireAuthentication: function  (req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function (req, res, next) {
-		console.log("request: " + req.method + " " + req.originalUrl + new Date().toString());
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 // app.use(middleware.requireAuthentication);
